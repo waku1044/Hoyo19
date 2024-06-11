4a1b2c10faa7b3093e815f708c999a9e739a64f2
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2024-06-11 12:44:01 -0300

    se modifica en js la resolucion de pantallas mas de 992 solo eventos en el navbar

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const links = document.querySelectorAll(".nav-link");
 const menu = document.querySelector("#navbarSupportedContent");
+const navEvento = document.querySelector(".navCatering");
 links.forEach((link) => {
     link.addEventListener("click", (e) => {
        
             menu.classList.toggle("show");   
     });
 })
+
+if(window.innerWidth >= 992){
+    navEvento.innerText = "Eventos";
+
+}
