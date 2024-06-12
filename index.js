2c99945cddb20d0843c086469fa5c3e46ff517bf
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2024-06-12 13:01:35 -0300

    se cambio lenguaje a español paara no tener complicaciones

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const links = document.querySelectorAll(".nav-link");
 const menu = document.querySelector("#navbarSupportedContent");
 const navEvento = document.querySelector(".navCatering");
-const kiosco = document.querySelector(".kiosco");
 
-kiosco.innerText = "Kiosco";
 links.forEach((link) => {
     link.addEventListener("click", (e) => {
        
